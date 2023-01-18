2699cadb89ea0031d1f06069a0e72baf0633ed1e
jisoolee11 <jisu6414@likelion.org>
2023-01-18 22:04:37 +0900

    프로젝트 폴더구조 초기세팅

---
+++ src/main/java/com/likelionsyu11th/likelionsyu11th/LikelionSyu11thApplication.java
@@ -0,0 +1,13 @@
+package com.likelionsyu11th.likelionsyu11th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionSyu11thApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionSyu11thApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelionsyu11th/likelionsyu11th/LikelionSyu11thApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelionsyu11th.likelionsyu11th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionSyu11thApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
